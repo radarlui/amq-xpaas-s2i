8d51903d6ae67ab839677607551eda4515f50f3d
radarlui <radarlui@users.noreply.github.com>
2016-10-22 00:12:26 +0800

    Update configureCPA.sh

--- amq-ssl-s2i/configuration/configureCPA.sh
+++ amq-ssl-s2i/configuration/configureCPA.sh
@@ -1,3 +1,3 @@
 sed -i "s|<transportConnector name=\"ssl\" uri=\"ssl://0.0.0.0:61617?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\" \/>|<transportConnector name=\"ssl\" uri=\"ssl://0.0.0.0:61617?needClientAuth=true\&amp;maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\" \/>|" $AMQ_HOME/conf/activemq.xml
 
-export ACTIVEMQ_USER_CLASSPATH=/opt/amq/conf/mysql-connector-java-5.1.36.jar:/opt/amq/conf/commons-pool2-2.4.2.jar
+export ACTIVEMQ_CLASSPATH=/opt/amq/lib
